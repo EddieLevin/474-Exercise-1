1767faed04eb0f9e394b701f6103342d3b2fa3e1
EddieLevin <edlevin@udel.edu>
2020-09-08 13:11:44 -0400

    Added ;

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 function print(n) {
-    let outList = []
+    let outList = [];
     for (i = 0; i < n; i++) {
-        outList.push(i)
+        outList.push(i);
     }
     outList.forEach(element => {
-        console.log(element)
+        console.log(element);
     });
 }
-print(4)
+print(4);
