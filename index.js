685f6ccce1cb814b8ca78d458a94be98d9086af2
EddieLevin <edlevin@udel.edu>
2020-09-09 11:26:35 -0400

    reversed string now displays!

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-$(function(){
-    //this code runs after page is fully loaded
+$(function () {
+    $("#nameform").submit(function (event) {
+        event.preventDefault();
+        var fname = $('#fname').val();
+        var lname = $('#lname').val();
+        $('.jumbotron').text(reverse(fname+lname));
+        $('.jumbotron').attr("style", "display: block !important;");
+        return false;
     });
+});
+
+function reverse(str) {
+    return str.split("").reverse().join("");
+}
+
